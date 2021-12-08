1014c89be1eac738621832e4abf4b24b35ad3ce6
Shlok D. Mange <shlok_mange@protonmail.com>
2021-12-08 17:25:15 +0530

    Basic UI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
 import '../styles/globals.css'
+import '@fontsource/readex-pro'
+import '@fontsource/noto-sans'
 import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
+import { DAppProvider } from '@usedapp/core'
+
+const config = {
+  autoConnect: false
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <DAppProvider config={config}>
+        <Component {...pageProps} />
+      </DAppProvider>
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
